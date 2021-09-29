d236b35616ca23fac9b82e17ddc44e7764205d68
Kamil Duliniec <123dulko@wp.pl>
2021-09-29 22:58:31 +0200

    add functional drag n drop, change mind about localstorage for another idea

--- app/api.js
+++ app/api.js
@@ -1,47 +1,5 @@
 function schedule() {
-  function getItem(col) {
-    const column = read().find((el) => el.id == col);
-    if (!column) {
-      return [];
-    }
-    return column.items;
-  }
-
-  function insert(col, content) {
-    const data = read();
-    const column = data.find((el) => el.id == col);
-    const item = {
-      id: Math.floor(Math.random() * 1000),
-      content,
-    };
-    if (!column) {
-      throw new Error("Column doesn't exist");
-    }
-    column.items.push(item);
-    save(data);
-  }
-
-  function update(itemUpdate, newInfo) {
-    const data = read();
-    const [item, currentCol] = (() => {
-      for (const col of data) {
-        const item = col.items.find((el) => el.id == itemUpdate);
-
-        if (item) {
-          return [item, col];
-        }
-      }
-    })();
-
-    if (!item) {
-      throw new Error("Item not found");
-    }
-
-    item.content =
-      newInfo.content === undefined ? item.content : newInfo.content;
-  }
-
-  function save(data) {
+  const save = (data) => {
     localStorage.setItem("data", JSON.stringify(data));
   }
 
@@ -67,8 +25,8 @@ function schedule() {
   }
 
   return {
-    insert,
-    update,
+    read,
+    save
   };
 }
 
--- app/app.js
+++ app/app.js
@@ -1,2 +1,39 @@
-import schedule from "./api.js";
+const item_input = document.querySelectorAll(".items-input");
+const lists = document.querySelectorAll(".list");
 
+let draggedItem = null;
+
+for (let x = 0; x < item_input.length; x++) {
+  const item = item_input[x];
+
+  item.addEventListener("dragstart", () => {
+    console.log("dragged");
+    draggedItem = item;
+    setTimeout(function () {
+      item.style.display = "none";
+    }, 0);
+  });
+
+  item.addEventListener("dragend", () => {
+    console.log("dragend");
+    setTimeout(function () {
+      draggedItem.style.display = "block";
+    }, 0);
+  });
+
+  for (let z = 0; z < lists.length; z++) {
+    const list = lists[z];
+
+    list.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+
+    list.addEventListener("dragenter", (e) => {
+      e.preventDefault();
+    });
+
+    list.addEventListener("drop", (e) => {
+      list.append(draggedItem);
+    });
+  }
+}
